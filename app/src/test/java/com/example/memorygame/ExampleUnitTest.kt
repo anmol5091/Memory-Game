4138c6fe46df27f9c93a1ab2c3bcf98676f3d0e1
anmol5091 <60506652+anmol5091@users.noreply.github.com>
2020-04-24 11:59:26 +0530

    Update ExampleUnitTest.kt

--- app/src/test/java/com/example/memorygame/ExampleUnitTest.kt
+++ app/src/test/java/com/example/memorygame/ExampleUnitTest.kt
@@ -2,13 +2,8 @@ package com.example.memorygame
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
